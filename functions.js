e7c6c8ecb031c25f84d80a6112989ff92a076fc7
Maksimiljan-Galos <maxvonlaibach@gmail.com>
2022-02-26 12:12:34 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,10 @@
+const sayHello = () => console.log("Hello World 2");
+
+const add = (a,b) => { return (a+b) }
+
+module.exports = {
+    sayHello,
+    add
+
+    //jeśli nazwy są inne od argumentów, należy je tu wpisać po dwukropku. 
+}
